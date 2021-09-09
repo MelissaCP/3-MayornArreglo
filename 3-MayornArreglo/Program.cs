8fc204d6c3e43297802479667c305e40051ed76a
Melicha <Melicha@DELL-P5520>
2021-09-08 19:21:08 -0700

    Agregar archivos de proyecto.

---
+++ 3-MayornArreglo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using C = System.Console;
+
+namespace _3_MayornArreglo
+{
+    /// Practica 3
+    /// 3.- Encontrar el mayor de n números en un arreglo.
+
+    /// Chávez Pérez Melissa
+    /// #M21210035
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C.BackgroundColor = ConsoleColor.Blue;
+            C.ForegroundColor = ConsoleColor.White;
+
+            C.Write("\nFavor de Ingresar cuantos numeros desea comparar ");
+            int v;
+            v = int.Parse(C.ReadLine());
+
+            // Declaracion de Arreglo 
+            int[] datos = new int[v];
+            int i, mayor = 0, j= 0;
+            for(i=0; i < v; i++)
+            {
+                C.WriteLine("\nIngrese valor {0}",(i+1));
+                datos[i] = Convert.ToInt32(C.ReadLine());
+            }
+            while (j<v)
+            {
+                if (datos[j] > mayor)
+                    mayor = datos[j];
+                j++;
+            }
+            C.WriteLine("El mayor de los datos es: {0}", mayor);
+            C.ReadLine();
+
+        }
+    }
+}
